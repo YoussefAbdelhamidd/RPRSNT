12675789e150a33e7ba58aacfdb3bec502546512
YoussefAbdelhamidd <youssefabdelhamid041@gmail.com>
2026-03-16 16:55:24 +0200

    fuck morsi

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import { LoginPage, DashboardPage } from './pages'
-import { hasSavedAccessSession, revokeAccessSession } from './utils'
+// import { useState } from 'react'
+// import { LoginPage, DashboardPage } from './pages'
+// import { hasSavedAccessSession, revokeAccessSession } from './utils'
 import { TemporaryDownPage } from './pages/TemporaryDownPage'
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(hasSavedAccessSession)
+  // const [isAuthenticated, setIsAuthenticated] = useState(hasSavedAccessSession)
 
-  const handleAuthenticated = () => setIsAuthenticated(true)
+  // const handleAuthenticated = () => setIsAuthenticated(true)
 
-  const handleLogout = () => {
-    revokeAccessSession()
-    setIsAuthenticated(false)
-  }
+  // const handleLogout = () => {
+  //   revokeAccessSession()
+  //   setIsAuthenticated(false)
+  // }
 
   // Temporary maintenance mode: always show "site down" screen
   return <TemporaryDownPage />
